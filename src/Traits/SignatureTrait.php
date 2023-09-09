74f9646e03c74d2767ad337fa5b8d386017569a6
chenkby <99912250@qq.com>
2023-09-09 10:04:28 +0800

    Fix PHP8.1 Deprecated:Passing null to parameter #1 ($datetime) of type string is deprecated

--- src/Traits/SignatureTrait.php
+++ src/Traits/SignatureTrait.php
@@ -23,6 +23,6 @@ trait SignatureTrait
     public function gmt_iso8601($time)
     {
         // fix bug https://connect.console.aliyun.com/connect/detail/162632
-        return (new \DateTime(null, new \DateTimeZone('UTC')))->setTimestamp($time)->format('Y-m-d\TH:i:s\Z');
+        return (new \DateTime('', new \DateTimeZone('UTC')))->setTimestamp($time)->format('Y-m-d\TH:i:s\Z');
     }
 }
